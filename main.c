d031379939ddb072d265984f85953deb8bc4b483
Ryan Lamo <c14ryan.lamo@usafa.edu>
2013-12-13 08:39:46 -0700

    initial left wall following program. stops if center sensor sees wall

---
+++ main.c
@@ -0,0 +1,54 @@
+/* Author: C1C Ryan Lamo
+ * Created: 11 December 2013
+ * Last Edited: 11 December 2013
+ * Description: This code will program the robot to navigate a maze in a left-wall following fashion.
+ * Documentation:
+ */
+
+#include "Lab7/sensor.h"
+#include "Lab6/robot.h"
+#include <msp430.h> 
+
+//unsigned int leftSensor, rightSensor, centerSensor;
+
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+
+    initPWM();
+    initADC();
+
+    for(;;)				//Left wall following program
+    {
+    	if (getCenterSensorReading() > 0x1ff)
+    	{
+    		stopRobot();
+
+
+    	}
+    	else if (getLeftSensorReading() > 0x1a0)
+    	{
+    		moveRobotRight();
+    		__delay_cycles(10000);
+    	}
+    	else if (getRightSensorReading() > 0x1a0)
+    	{
+    		moveRobotLeft();
+    		__delay_cycles(10000);
+    	}
+    	else
+    	{
+    		moveRobotForward();
+    		__delay_cycles(10000);
+    	}
+
+
+    }
+	
+	return 0;
+}
+
+#pragma vector=ADC10_VECTOR
+__interrupt void ADC10_ISR(void)
+{
+	__bic_SR_register_on_exit(CPUOFF);
+}
